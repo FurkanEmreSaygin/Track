4c2d4d778b3c8f554c7a3fd9cd4ad15d8d9ca521
furkan emre saygin <furkanemresaygi@gmail.com>
2025-05-28 19:59:03 +0300

    Add Show entty and setup DbContext wth correct project reference

---
+++ TrackFlix.Domain/Show.cs
@@ -0,0 +1,18 @@
+﻿namespace TrackFlix.Domain;
+
+public class Show
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime? ReleaseDate { get; set; }
+    public int? Episodes { get; set; }
+    public string? PosterUrl { get; set; }
+    public double? IMDB { get; set; }
+
+    public bool IsPublished { get; set; } = false;
+    public bool IsFeatured { get; set; } = false;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+}
---
+++ TrackFlix.Infrastructure/Data/TrackFlixDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TrackFlix.Domain;
+
+namespace TrackFlix.Infrastructure.Data
+{
+    public class TrackFlixDbContext : DbContext
+    {
+        public TrackFlixDbContext(DbContextOptions<TrackFlixDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Show> Shows { get; set; }
+
+    }
+}
